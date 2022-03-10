af28569798066c4a5b4a2fb7155e29d4f3e4da03
Kelly Keng <kelly.keng1@gmail.com>
2022-03-10 14:10:24 -0500

    trainer code

--- newtimeclock.sh
+++ newtimeclock.sh
@@ -4,14 +4,18 @@
 # Timeclock Program
 
 echo "Running newtimeclock.sh..."
+timestamp =$(date +"%Y-%m-%d %T")
+
 
 while [ true ]; 
+
 	do
 	read -p $'this is my prompt: \n' input 
 	echo $input
+	echo $timestamp
 
-	if [[ $input =~ /stop/i ]]; then	
-#	if [[ $input == "STOP" ]]; then
+#	if [[ $input =~ /stop/i ]]; then	
+	if [[ $input == "STOP" ]]; then
 		break
 	elif [[ -z $input ]]; then
 		echo "empty input found, try again"	
